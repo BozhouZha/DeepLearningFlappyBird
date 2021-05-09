f6addb7db7ade4c89454bfed10403ce8dcb58040
zhuheng <zhuheng@seas.upenn.edu>
2021-05-08 22:32:39 -0400

    Add the evaluation process to the training and modified the rewards.

--- deep_q_network.py
+++ deep_q_network.py
@@ -11,19 +11,22 @@ import numpy as np
 from collections import deque
 from create_network import createNetwork
 import matplotlib.pyplot as plt
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
 
 GAME = 'bird' # the name of the game being played for log files
 ACTIONS = 2 # number of valid actions
 GAMMA = 0.99 # decay rate of past observations
-OBSERVE = 100000. # timesteps to observe before training
-EXPLORE = 2000000. # frames over which to anneal epsilon
+OBSERVE = 2000. # timesteps to observe before training
+EXPLORE = 3000000. # frames over which to anneal epsilon
 FINAL_EPSILON = 0.0001 # final value of epsilon
-INITIAL_EPSILON = 0.0001 # starting value of epsilon
+INITIAL_EPSILON = 0.1 # starting value of epsilon
 REPLAY_MEMORY = 50000 # number of previous transitions to remember
 BATCH = 32 # size of minibatch
 FRAME_PER_ACTION = 1
 
-def trainNetwork(s, readout, sess):
+def trainNetwork(s, readout, h_fc1, sess):
     # define the cost function
     a = tf.placeholder("float", [None, ACTIONS])
     y = tf.placeholder("float", [None])
@@ -37,6 +40,10 @@ def trainNetwork(s, readout, sess):
     # store the previous observations in replay memory
     D = deque()
 
+    # printing
+    a_file = open("logs_" + GAME + "/readout.txt", 'w')
+    h_file = open("logs_" + GAME + "/hidden.txt", 'w')
+
     # get the first state by doing nothing and preprocess the image to 80x80x4
     do_nothing = np.zeros(ACTIONS)
     do_nothing[0] = 1
@@ -49,80 +56,42 @@ def trainNetwork(s, readout, sess):
     saver = tf.train.Saver()
     sess.run(tf.initialize_all_variables())
     checkpoint = tf.train.get_checkpoint_state("saved_networks")
-    if checkpoint and checkpoint.model_checkpoint_path:
-        saver.restore(sess, checkpoint.model_checkpoint_path)
-        print("Successfully loaded:", checkpoint.model_checkpoint_path)
-    else:
-        print("Could not find old network weights")
+    # if checkpoint and checkpoint.model_checkpoint_path:
+    #     saver.restore(sess, checkpoint.model_checkpoint_path)
+    #     print("Successfully loaded:", checkpoint.model_checkpoint_path)
+    # else:
+    #     print("Could not find old network weights")
 
     # start training
     epsilon = INITIAL_EPSILON
     t = 0
-    # count the fps
-    fps_count = 1
-    min_count = 0
-    score_permin = []
-    mean_score = []
-    cur_score = [0]
-
-    # instantly plot
-    plt.ion()
-    # plt.figure(1)
+    reward_saver = []
     while "flappy bird" != "angry bird":
-
-        # ================ plotting part: Zhuheng =============
         # choose an action epsilon greedily
-        # if fps_count == 2000:    # 1 mins 2000
-        #     score_permin.append(sum(cur_score))
-        #     mean_score.append(float(sum(cur_score)/len(cur_score)))
-        #     min_count += 1
-        #
-        #     # draw the plot instantly
-        #     plt.figure(1)
-        #     plt.clf()
-        #     plt.title('Total scores every 2000 fps (per minute)')
-        #     plt.xlabel('minutes')
-        #     plt.ylabel('scores')
-        #     plt.plot(np.arange(1, min_count+1), np.array(score_permin), 'rx-')
-        #
-        #
-        #     plt.figure(2)
-        #     plt.clf()
-        #     plt.title('Average scores per round every 2000 fps (per minute)')
-        #     plt.xlabel('minutes')
-        #     plt.ylabel('every scores')
-        #     plt.plot(np.arange(1, min_count + 1), np.array(mean_score), 'rx-')
-        #
-        #     plt.pause(0.01)
-        #
-        #     # reset the current score list and count
-        #     cur_score = [0]
-        #     fps_count = 1
-
-        readout_t = readout.eval(feed_dict={s: [s_t]})[0]
+        readout_t = readout.eval(feed_dict={s : [s_t]})[0]
         a_t = np.zeros([ACTIONS])
         action_index = 0
         if t % FRAME_PER_ACTION == 0:
-            if random.random() <= epsilon:  # epsilon-greedy: <e uniform action
+            if random.random() <= epsilon:
                 print("----------Random Action----------")
                 action_index = random.randrange(ACTIONS)
-                a_t[random.randrange(ACTIONS)] = 1  # initialization at the random action
+                a_t[random.randrange(ACTIONS)] = 1
             else:
                 action_index = np.argmax(readout_t)
                 a_t[action_index] = 1
         else:
-            a_t[0] = 1  # do nothing
+            a_t[0] = 1 # do nothing
 
         # scale down epsilon
         if epsilon > FINAL_EPSILON and t > OBSERVE:
             epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
 
         # run the selected action and observe next state and reward
-        x_t1_colored, r_t, terminal = game_state.frame_step(a_t)  # r_t saved 100 step
+        x_t1_colored, r_t, terminal = game_state.frame_step(a_t)
         x_t1 = cv2.cvtColor(cv2.resize(x_t1_colored, (80, 80)), cv2.COLOR_BGR2GRAY)
         ret, x_t1 = cv2.threshold(x_t1, 1, 255, cv2.THRESH_BINARY)
         x_t1 = np.reshape(x_t1, (80, 80, 1))
-        # s_t1 = np.append(x_t1, s_t[:,:,1:], axis = 2)
+        #s_t1 = np.append(x_t1, s_t[:,:,1:], axis = 2)
         s_t1 = np.append(x_t1, s_t[:, :, :3], axis=2)
 
         # store the transition in D
@@ -142,7 +111,7 @@ def trainNetwork(s, readout, sess):
             s_j1_batch = [d[3] for d in minibatch]
 
             y_batch = []
-            readout_j1_batch = readout.eval(feed_dict={s: s_j1_batch})
+            readout_j1_batch = readout.eval(feed_dict = {s : s_j1_batch})
             for i in range(0, len(minibatch)):
                 terminal = minibatch[i][4]
                 # if terminal, only equals reward
@@ -152,19 +121,29 @@ def trainNetwork(s, readout, sess):
                     y_batch.append(r_batch[i] + GAMMA * np.max(readout_j1_batch[i]))
 
             # perform gradient step
-            train_step.run(feed_dict={
-                y: y_batch,
-                a: a_batch,
-                s: s_j_batch}
+            train_step.run(feed_dict = {
+                y : y_batch,
+                a : a_batch,
+                s : s_j_batch}
             )
+
+            # evaluate network every 5000 timesteps
+            if t%1000==0:
+                average_reward = evaluateNetwork(s,readout)
+                reward_saver.append(average_reward)
 
         # update the old values
         s_t = s_t1
         t += 1
 
-        # save progress every 10000 iterations
+        # save progress every 10000 iterations and plot the average reward
         if t % 10000 == 0:
-            saver.save(sess, 'saved_networks/' + GAME + '-dqn', global_step=t)
+            saver.save(sess, 'saved_networks/' + GAME + '-dqn', global_step = t)
+            fig, ax = plt.subplots(1)
+            ax.plot(range(len(reward_saver)), reward_saver)
+            ax.set_xlabel('time steps/1000')
+            ax.set_ylabel('average_reward')
+            fig.savefig('images/reward_images/average_reward_'+str(t)+'.png')
 
         # print info
         state = ""
@@ -176,23 +155,65 @@ def trainNetwork(s, readout, sess):
             state = "train"
 
         print("TIMESTEP", t, "/ STATE", state, \
-              "/ EPSILON", epsilon, "/ ACTION", action_index, "/ REWARD", r_t, \
-              "/ Q_MAX %e" % np.max(readout_t))
-        game_score = game_state.score
-        print(game_score)
+            "/ EPSILON", epsilon, "/ ACTION", action_index, "/ REWARD", r_t, \
+            "/ Q_MAX %e" % np.max(readout_t))
+        # write info to files
+        '''
+        if t % 10000 <= 100:
+            a_file.write(",".join([str(x) for x in readout_t]) + '\n')
+            h_file.write(",".join([str(x) for x in h_fc1.eval(feed_dict={s:[s_t]})[0]]) + '\n')
+            cv2.imwrite("logs_tetris/frame" + str(t) + ".png", x_t1)
+        '''
 
-        # record the current minute score ============== Zhuheng
-        # if game_score != 0 and game_score != cur_score[-1]:
-        #     cur_score[-1] = game_score
-        # elif game_score == 0 and cur_score[-1] != 0:
-        #     cur_score.append(0)
-        # fps_count += 1    # count the fps
+
+def evaluateNetwork(s, readout):
+    print('start evaluation')
+    n_trails = 10
+    max_timestep = 2000
+    # run n trials and calculate the average score
+    average_reward = []
+    for i in range(n_trails):
+        game_state = game.GameState()
+        # get the first state by doing nothing and preprocess the image to 80x80x4
+        do_nothing = np.zeros(ACTIONS)
+        do_nothing[0] = 1
+        x_t, r_0, terminal = game_state.frame_step(do_nothing)
+        x_t = cv2.cvtColor(cv2.resize(x_t, (80, 80)), cv2.COLOR_BGR2GRAY)
+        ret, x_t = cv2.threshold(x_t, 1, 255, cv2.THRESH_BINARY)
+        s_t = np.stack((x_t, x_t, x_t, x_t), axis=2)
+
+        t = 0
+        reward_list = []
+        while (not terminal) and (t<max_timestep):
+            # choose an action epsilon greedily
+            readout_t = readout.eval(feed_dict={s : [s_t]})[0]
+            a_t = np.zeros([ACTIONS])
+            if t % FRAME_PER_ACTION == 0:
+                action_index = np.argmax(readout_t)
+                a_t[action_index] = 1
+            else:
+                a_t[0] = 1 # do nothing
+            x_t1_colored, r_t, terminal = game_state.frame_step(a_t)
+            reward_list.append(r_t)
+            x_t1 = cv2.cvtColor(cv2.resize(x_t1_colored, (80, 80)), cv2.COLOR_BGR2GRAY)
+            ret, x_t1 = cv2.threshold(x_t1, 1, 255, cv2.THRESH_BINARY)
+            x_t1 = np.reshape(x_t1, (80, 80, 1))
+            # s_t1 = np.append(x_t1, s_t[:,:,1:], axis = 2)
+            s_t1 = np.append(x_t1, s_t[:, :, :3], axis=2)
+
+            s_t = s_t1
+            t += 1
+        average_reward.append(sum(reward_list)/len(reward_list))
+    print('stop evaluation and the average reward is '+ str(sum(average_reward)/len(average_reward)))
+    return sum(average_reward)/len(average_reward)
+
+
 
 
 def playGame():
     sess = tf.InteractiveSession()
     s, readout, h_fc1 = createNetwork()
-    trainNetwork(s, readout, sess)
+    trainNetwork(s, readout, h_fc1, sess)
 
 if __name__ == "__main__":
     playGame()
